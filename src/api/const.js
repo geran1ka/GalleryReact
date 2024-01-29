c5b5db340a96b47c18811f55ef4cb7a76ece04fd
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-01-29 22:37:54 +0300

    change redirect_uri

--- src/api/const.js
+++ src/api/const.js
@@ -1,6 +1,7 @@
 export const API_URL_AUTH = "https://unsplash.com/oauth/authorize?";
 // export const REDIRECT_URI = "http://localhost:5173/auth";
-export const REDIRECT_URI = "https://gallery-react-one.vercel.app/auth";
+// export const REDIRECT_URI = "https://gallery-react-one.vercel.app/auth";
+export const REDIRECT_URI = "https://glittery-duckanoo-f8a1c7.netlify.app/auth";
 
 export const RESPONSE_TYPE = "code";
 export const SCOPE = "public read_user read_photos write_likes";
@@ -10,13 +11,13 @@ export const GRANT_TYPE = "authorization_code";
 
 export const API_URL = "https://api.unsplash.com";
 
-export const APPLICATION_ID = "554637";
-export const SECRET_KEY = "Q1nzMywrfQX9LOdOTubXmAMCPBvKeaEFcWQmKBi54tg";
-export const ACCESS_KEY = "vwALTE2T2Bu2PGvLsalBrl04ZAU2g0QCg4fZm9nXsfQ";
+// export const APPLICATION_ID = "554637";
+// export const SECRET_KEY = "Q1nzMywrfQX9LOdOTubXmAMCPBvKeaEFcWQmKBi54tg";
+// export const ACCESS_KEY = "vwALTE2T2Bu2PGvLsalBrl04ZAU2g0QCg4fZm9nXsfQ";
 
-// export const APPLICATION_ID = "559426";
-// export const ACCESS_KEY = "mvKr5pokxhhfMW9t7kkzpdN14NxsnRA2JGlyS9yj-E4";
-// export const SECRET_KEY = "MCr-yo8YFx2mqdj7A0iGji7AAbYmQxbDHLNQn3tAHEY";
+export const APPLICATION_ID = "559426";
+export const ACCESS_KEY = "mvKr5pokxhhfMW9t7kkzpdN14NxsnRA2JGlyS9yj-E4";
+export const SECRET_KEY = "MCr-yo8YFx2mqdj7A0iGji7AAbYmQxbDHLNQn3tAHEY";
 
 // export const APPLICATION_ID = "559439";
 // export const SECRET_KEY = "VngRQp6CTU-kfJArLDOZag4XlJ6wlnw8BQ23jKTIbNs";
