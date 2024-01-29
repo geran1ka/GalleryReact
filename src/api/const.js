3423a5f459b2a6000a7568cea55dc7f391b024f9
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-01-29 21:57:29 +0300

    change redirect_uri

--- src/api/const.js
+++ src/api/const.js
@@ -1,5 +1,7 @@
 export const API_URL_AUTH = "https://unsplash.com/oauth/authorize?";
-export const REDIRECT_URI = "http://localhost:5173/auth";
+// export const REDIRECT_URI = "http://localhost:5173/auth";
+export const REDIRECT_URI = "https://gallery-react-one.vercel.app/auth";
+
 export const RESPONSE_TYPE = "code";
 export const SCOPE = "public read_user read_photos write_likes";
 
