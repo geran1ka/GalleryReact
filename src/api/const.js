ff9b50022f285a3d719859e0f5f2d9e35eaa9400
Geran1ka <roman.khoruzhy@gmail.com>
2024-01-30 11:05:25 +0300

    add homepage

--- src/api/const.js
+++ src/api/const.js
@@ -1,5 +1,5 @@
 export const API_URL_AUTH = "https://unsplash.com/oauth/authorize?";
-// export const REDIRECT_URI = "http://localhost:5173/auth";
+export const REDIRECT_URI = "http://localhost:4173/auth";
 export const REDIRECT_URI =
   "https://gallery-dad5322ph-geran1ka.vercel.app/auth";
 // export const REDIRECT_URI = "https://glittery-duckanoo-f8a1c7.netlify.app/auth";
@@ -12,13 +12,13 @@ export const GRANT_TYPE = "authorization_code";
 
 export const API_URL = "https://api.unsplash.com";
 
-export const APPLICATION_ID = "554637";
-export const SECRET_KEY = "Q1nzMywrfQX9LOdOTubXmAMCPBvKeaEFcWQmKBi54tg";
-export const ACCESS_KEY = "vwALTE2T2Bu2PGvLsalBrl04ZAU2g0QCg4fZm9nXsfQ";
+// export const APPLICATION_ID = "554637";
+// export const SECRET_KEY = "Q1nzMywrfQX9LOdOTubXmAMCPBvKeaEFcWQmKBi54tg";
+// export const ACCESS_KEY = "vwALTE2T2Bu2PGvLsalBrl04ZAU2g0QCg4fZm9nXsfQ";
 
-// export const APPLICATION_ID = "559426";
-// export const ACCESS_KEY = "mvKr5pokxhhfMW9t7kkzpdN14NxsnRA2JGlyS9yj-E4";
-// export const SECRET_KEY = "MCr-yo8YFx2mqdj7A0iGji7AAbYmQxbDHLNQn3tAHEY";
+export const APPLICATION_ID = "559426";
+export const ACCESS_KEY = "mvKr5pokxhhfMW9t7kkzpdN14NxsnRA2JGlyS9yj-E4";
+export const SECRET_KEY = "MCr-yo8YFx2mqdj7A0iGji7AAbYmQxbDHLNQn3tAHEY";
 
 // export const APPLICATION_ID = "559439";
 // export const SECRET_KEY = "N1FRQB8WDxajGduVsihx2gvSboIyyh26Gtif62WbnE0";
